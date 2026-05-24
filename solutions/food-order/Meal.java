40167b4ea6d236b56829b4b003d068c800483a06
Maryam Skaik <mariamskaik2003@gmail.com>
2026-05-24 07:52:56 +0300

    Create Meal interface for meal types
    Added Meal interface for meal preparation.

---
+++ solutions/food-order/Meal.java
@@ -0,0 +1,13 @@
+/*
+ * Meal.java
+ *
+ * Interface representing a generic meal.
+ * All meal types (Burger, Pizza, Salad) must implement this interface.
+ */
+
+package FoodOrderSystem;
+
+public interface Meal {
+
+    void prepare(); // method to prepare the meal
+}
